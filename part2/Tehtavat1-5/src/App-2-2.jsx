4791d878d71045f08fa40574063244eaae4ba65f
joemus <joemus@utu.fi>
2024-02-29 13:39:07 +0200

    Tehtavat1-8 tehty

---
+++ part2/Tehtavat1-5/src/App-2-2.jsx
@@ -0,0 +1,80 @@
+/* eslint-disable react/prop-types */
+const Header = ({header}) => {
+  return(
+    <div>
+    <h1>
+      {header}
+    </h1>
+    </div>
+  )
+}
+const Part = (props) => {
+  return (
+    <div>
+    <p>{props.course.name} {props.course.exercises}</p>
+    </div>
+  )
+}
+const Content = (props) => {
+  return (
+    <div>
+    {props.kurssit.map((k) => <Part key={k.id} course={k}/>)}
+    </div>
+  )
+}
+
+const Total = (props) => {
+  console.log(props)
+  let total = 0
+  for(let i = 0; i < props.parts.length; i++){
+    total = total + props.parts[i].exercises
+  }
+
+  return (
+    <div>
+      <p>Total of {total} exercises</p>
+    </div>
+  )
+}
+  
+const Course = ({course}) => {
+  return(
+  <div>
+  <Header header={course.name} />
+  <Content kurssit={course.parts}/>
+  <Total parts={course.parts}/>
+  </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course}/>
+    </div>
+  )
+}
+
+export default App
