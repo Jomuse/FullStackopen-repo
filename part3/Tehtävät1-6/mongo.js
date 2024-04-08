ed9c806d5bcf27d17a321d2701fc8bc927384a8f
joemus <joemus@utu.fi>
2024-04-08 11:00:13 +0300

    part4 aloitettu

--- part3/Tehtävät1-6/mongo.js
+++ part3/Tehtävät1-6/mongo.js
@@ -6,8 +6,8 @@ if (process.argv.length<3) {
 }
 
 const password = process.argv[2]
-const name = process.argv[2]
-const number = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
 
 const url =
   `mongodb+srv://jomus:${password}@cluster0.kntoprx.mongodb.net/personApp?retryWrites=true&w=majority&appName=Cluster0`
@@ -26,8 +26,19 @@ const person = new Person({
   name: `${name}`,
   number: `${number}`,
 })
-
-person.save().then(result => {
-  console.log(`added ${name} number ${number} to a phonebook`)
-  mongoose.connection.close()
+if(name == undefined || number == undefined){
+  Person
+  .find({})
+  .then(result => {
+    console.log("Phonebook:")
+    result.forEach(person => {
+      console.log(person.name, person.number)
+    })
+    mongoose.connection.close()
+  })
+}else{
+  person.save().then(result => {
+    console.log(`added ${name} number ${number} to a phonebook`)
+    mongoose.connection.close()
 })
+}
