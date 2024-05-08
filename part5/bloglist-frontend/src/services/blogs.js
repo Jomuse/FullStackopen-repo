9c3b6a8100688c56bd062803e790002efce13747
joemus <joemus@utu.fi>
2024-05-08 22:04:53 +0300

    part5 tehty

--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -11,7 +11,7 @@ const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
-  
+
 const create = async newObject => {
   const config = {
     headers: { Authorization: token },
@@ -19,20 +19,20 @@ const create = async newObject => {
   const response = await axios.post(baseUrl, newObject, config)
   return response.data
 }
-  
+
 const update = async (id, newObject) => {
   const config = {
-    headers: { Authorization: token},
+    headers: { Authorization: token },
   }
   const response = await axios.put(`${ baseUrl }/${id}`, newObject, config)
   return response.data
 }
 
-const remove = async (id)=> {
+const remove = async (id) => {
   const config = {
-    headers: { Authorization: token},
+    headers: { Authorization: token },
   }
-  const response = await axios.delete(`${ baseUrl }/${id}`, newObject, config)
+  const response = await axios.delete(`${ baseUrl }/${id}`, config)
   return response.data
 }
 
